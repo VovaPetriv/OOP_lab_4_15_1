7aee4f50a4a4ad109d358a6e6700fac3bf16d287
Роман Данилевич <dwordchanel@gmail.com>
2020-05-27 17:10:07 +0300

    Add project files.

---
+++ OOP_lab_4_15_1/Output.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_lab_4_15_1
+{
+    class Output
+    {
+        public static void CheckingStudentRating()
+        {
+            Console.WriteLine(Program.Roman.StudentRating());
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            OOP_lab_4_15_1.Program.Roman.Rating = 95;
+            Assert.AreEqual(OOP_lab_4_15_1.Program.Roman.StudentRating(), "\nВiтаємо вiдмiнника!");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            OOP_lab_4_15_1.Program.Roman.Rating = 80;
+            Assert.AreEqual(OOP_lab_4_15_1.Program.Roman.StudentRating(), "\nМожна вчитися краще!");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            OOP_lab_4_15_1.Program.Roman.Rating = 60;
+            Assert.AreEqual(OOP_lab_4_15_1.Program.Roman.StudentRating(), "\nВарто бiльше уваги придiляти навчанню!");
+        }
+    }
+}
